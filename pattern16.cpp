9ad1d7b87eff820112d6444547f8ac2cc583755e
Shashank_Tiwari <shashanktiwari238@gmail.com>
2021-02-24 22:45:00 +0530

    another commit

--- pattern16.cpp
+++ pattern16.cpp
@@ -12,7 +12,7 @@ int main()
     for(int i=1;i<=n;i++)
     {
     
-    
+    int x=1;
         for(int j=1;j<=k;j++)
         {
             cout<<" ";
@@ -22,7 +22,7 @@ int main()
 
         for(int j=1;j<=2*i-1;j++)
         {
-            cout<<"*";
+            cout<<x++;
         }
 
         cout<<endl;
@@ -31,14 +31,14 @@ int main()
     k=1;
     for(int i=1;i<=n-1;i++)
     {
-        
+        int x=1;
         for(int j=1;j<=k;j++)
         cout<<" ";
 
         k++;
         
         for(int j=1;j<=2*(n-i)-1;j++)
-            cout<<"*";
+            cout<<x++;
         cout<<endl;
     }
     return 0;
